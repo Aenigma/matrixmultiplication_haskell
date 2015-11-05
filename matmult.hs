86d828498f9262f51ee7904e497b11cbead32555
Kevin Raoofi <nullindev@gmail.com>
2015-11-05 08:12:32 -0500

    Copied functions to matmult.hs

--- matmult.hs
+++ matmult.hs
@@ -1,20 +1,30 @@
 import Data.Char
 import Data.List
 import System.IO
+import Data.Array
 
+-- sort 
+arrSwap :: (Ix t, Ix t1) => Array (t, t1) e -> (t, t1) -> (t, t1) -> Array (t, t1) e
+indexTrans :: Ix a => Array a e -> Integer -> a
+indexTrans arr i = (range $ bounds arr) !! (fromIntegral i)
+bSortIter :: Array (Integer, Integer) e -> [Integer]
+bSortHelper :: (Ord a, Ix t, Ix t1) =>
+       Array (t, t1) a -> [Integer] -> Array (t, t1) a
+
+-- main, uses IO monad
 main :: IO ()
 
+-- matrix parsing
 wordSplit :: String -> [String]
 wordsToInteger :: [String] -> [Integer]
-
-matMult :: Num a => [[a]] -> [[a]] -> [[a]]
-vecMatMult :: Num a => [a] -> [[a]] -> [a]
-
-matPrettyPrint :: Show a => [[a]] -> String
-
 splitN :: [a] -> Int -> ([a], [a])
 splitHalf :: [a] -> ([a], [a])
 splitNList :: [t] -> Int -> [[t]]
+
+-- matrix specific
+matMult :: Num a => [[a]] -> [[a]] -> [[a]]
+vecMatMult :: Num a => [a] -> [[a]] -> [a]
+matPrettyPrint :: Show a => [[a]] -> String
 
 matPrettyPrint mat = intercalate "\n"
   [ "[" ++ intercalate ", "
@@ -69,8 +79,26 @@ vecMatMult _ [] = []
 splitHalf l = splitN l 2
 splitN l n = splitAt (quot (length l) n) l
 
-splitNList [] _ = []
 
+arrSwap arr (m1x,m1y) (m2x,m2y) = arr//[((m1x,m1y), arr ! (m2x, m2y)), ((m2x,m2y), arr ! (m1x, m1y))]
+
+arrSize arr = (x2-x1+1) * (y2-y1+1)
+  where ((x1,y1), (x2,y2)) = bounds arr
+
+bSort arr = bSortHelper arr (bSortIter arr)
+
+bSortIter arr = take (sint * sint - 1) (cycle [0..s-2])
+  where s = arrSize arr
+        sint = fromIntegral s
+
+bSortHelper arr [] = arr
+bSortHelper arr (i:is)
+  | (arr ! (p1_1, p1_2)) > (arr ! (p2_1, p2_2)) = bSortHelper (arrSwap arr (p1_1, p1_2) (p2_1, p2_2)) is
+  | otherwise = bSortHelper arr is
+    where (p1_1, p1_2) = indexTrans arr i
+          (p2_1, p2_2) = indexTrans arr (i+1)
+
+splitNList [] _ = []
 -- doesn't work if length l is not divisible by n...
 splitNList l n = lres : splitNList ls n
   where (lres, ls) = splitAt n l
